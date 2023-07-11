be616a82f753327d596e3011d164a8b0c7370fe4
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-07-11 20:38:12 -0300

    updating contact and gallery

--- src/components/english/contact.tsx
+++ src/components/english/contact.tsx
@@ -1,5 +1,81 @@
+import { Contact, ContactContent, IconsLinks } from "../styles/contact";
+
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiOutlineTwitter,
+  AiOutlineMail,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
 function EnglishContact() {
-  return <h1>EnglishContact</h1>;
+  return (
+    <Contact>
+      <h1>Contact</h1>
+      <ContactContent>
+        <p>
+          I'm currently looking for a job opportunity, in case you're a dev, a
+          recruiter, or maybe just someone who wants to talk, fell free to reach
+          out - I would love to chat! You can reach me any hour through{" "}
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/gabriel-lavarini-moreira-878706149/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Linkedin
+          </a>{" "}
+          or via my personal email:{" "}
+          <a
+            className="mail"
+            href="mailto:lavarinimoreira@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            lavarinimoreira@gmail.com
+          </a>
+        </p>
+        <h3>Get in touch!</h3>
+        <IconsLinks>
+          <a
+            href="mailto:lavarinimoreira@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineMail />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gabriel-lavarini-moreira-878706149/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin />
+          </a>
+          <a
+            href="https://www.github.com/lavarinimoreira"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub />
+          </a>
+          <a
+            href="https://twitter.com/lavarinimoreira"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineTwitter />
+          </a>
+          <a
+            href="https://www.instagram.com/gabriel_lavarini/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineInstagram />
+          </a>
+        </IconsLinks>
+      </ContactContent>
+    </Contact>
+  );
 }
 
 export default EnglishContact;
---
+++ src/components/styles/contact.ts
@@ -0,0 +1,128 @@
+import { styled } from "styled-components";
+
+export const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+
+  margin-top: 8rem;
+  margin-bottom: 6rem;
+
+  h2 {
+    border-bottom: 2px solid ${({ theme }) => theme.colors.gray600};
+    margin-bottom: 0.7rem;
+    padding-bottom: 1rem;
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.gray500};
+    line-height: 1.5rem;
+    text-align: justify;
+    margin-top: 1rem;
+  }
+
+  h3 {
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  @media (max-width: 815px) {
+    margin-top: 20rem;
+  }
+
+  @media (max-width: 750px) {
+    h1 {
+      font-size: 3rem;
+    }
+
+    h2 {
+      font-size: 2.5rem;
+    }
+
+    p {
+      font-size: 1.1rem;
+      text-align: center;
+    }
+  }
+
+  @media (max-width: 500px) {
+    margin-top: 17rem;
+
+    h1 {
+      font-size: 2.5rem;
+    }
+
+    h2 {
+      font-size: 2rem;
+    }
+
+    h4 {
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const ContactContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 1rem;
+
+  width: 45rem;
+
+  border: 1px solid white;
+  box-sizing: border-box;
+  padding: 2rem;
+
+  .linkedin,
+  .mail {
+    color: ${({ theme }) => theme.colors.white};
+    text-decoration: none;
+
+    transition: 0.4s;
+    &:hover {
+      color: ${({ theme }) => theme.colors.red};
+    }
+  }
+
+  @media (max-width: 750px) {
+    width: 30rem;
+  }
+
+  @media (max-width: 500px) {
+    width: 20rem;
+  }
+`;
+
+export const IconsLinks = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+
+  margin: 0 0 2rem 0;
+  width: 45%;
+
+  a {
+    color: ${({ theme }) => theme.colors.red};
+    font-size: 2.5em;
+    transition: 0.4s;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.white};
+    }
+
+    @media (max-width: 500px) {
+      font-size: 1.8rem;
+    }
+  }
+
+  @media (max-width: 750px) {
+    width: 80%;
+  }
+`;
